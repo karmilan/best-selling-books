d7a80d523bd5bc773dfac684d0fb67bc8d9e6581
Your Name <delftkarmilan@gmail.com>
2021-01-21 20:23:24 +0530

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Component } from 'react';
+import axios from 'axios';
+import { Button, Table, Container, Navbar  } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+axios.defaults.baseURL = 'https://api.nytimes.com/';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      records: [],
+      record: { title: 'NA' }
+    };
+  }
+
+  componentWillMount(){
+    //get = () => {
+      
+      axios.get('/svc/books/v3/lists/best-sellers/history.json?api-key=vsEAg88ysz1U9kAMZprYH1VxJAmPibAA')
+        .then(result => {
+          console.log(result.data.results);
+          this.setState({ records: result.data.results })
+        });
+  }
+
+  render() {
+    const bookTem = this.state.records.length > 0 ? this.state.records.map((book, index) =>  (
+      <tr key={index}>
+         <td>{index + 1}</td>
+                <td>{book.title}</td>
+                <td><Button variant="primary" onClick={() => { this.view(book.id) }}>View</Button></td>
+      </tr>
+    )) : <h1>Loading...</h1>
+    // <div key={index}> {book.title}
+    
+    // <Button variant="primary" onClick={() => { this.view() }}>View</Button>{' '}
+    // </div>):<h1>No Data</h1>
+    return (
+      <div>
+
+        <Navbar expand="lg" variant="light" bg="dark">
+          <Container>
+            <Navbar.Brand href="http://localhost:3000" style={{color:"white"}}>Best-Selling Books</Navbar.Brand>
+          </Container>
+        </Navbar>
+
+        
+        <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Title</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* <tr>
+                <td></td>
+              </tr>        */}
+              {bookTem}
+            </tbody>
+          </Table>
+
+          <p className="footer">&copy; hSenid Mobile </p>
+      </div>
+    );
+  }
+  // get = () => {
+  //   axios.get('/svc/books/v3/lists/best-sellers/history.json?api-key=vsEAg88ysz1U9kAMZprYH1VxJAmPibAA')
+  //     .then(result => {
+  //       console.log(result.data.results);
+  //       this.setState({ records: result.data.results })
+  //     })
+    
+  // }
+
+  view = (id) => {
+    axios.get('/svc/books/v3/lists/overview.json?api-key=vsEAg88ysz1U9kAMZprYH1VxJAmPibAA')
+      .then(result => {
+        console.log(result.data.results);
+        this.setState({ records: result.data.results })
+      })
+    
+  }
 }
+
+
 
 export default App;
---
+++ src/header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class topp extends Component{
+    render(){
+        return <h1>yyyy</h1>
+    }
+}
+
+export default topp;
